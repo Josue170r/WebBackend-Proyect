0497646e96c51f1c32ace04b477139396eee8349
Carlos Moreno <cmorenoh2000@alumno.ipn.mx>
2024-11-19 22:21:45 -0600

    Correción de import en repository de Proveedores

--- src/main/java/org/example/proyecto_productos/Proveedores/repository/ProveedoresRepository.java
+++ src/main/java/org/example/proyecto_productos/Proveedores/repository/ProveedoresRepository.java
@@ -1,6 +1,6 @@
 package org.example.proyecto_productos.Proveedores.repository;
 
-import org.example.proyecto_productos.domain.entities.Proveedores;
+import org.example.proyecto_productos.Proveedores.models.Proveedores;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProveedoresRepository extends CrudRepository<Proveedores, Long> {
