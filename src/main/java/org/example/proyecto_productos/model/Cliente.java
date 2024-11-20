03f622cebe86c9099d45b6ad0df475bfb031aac1
JerryOrtiz <mruriel982@gmail.com>
2024-11-20 10:12:36 -0600

    Codigo de clientes-model

---
+++ src/main/java/org/example/proyecto_productos/model/Cliente.java
@@ -0,0 +1,58 @@
+package org.example.proyecto_productos.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "Clientes")
+public class Cliente implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer idCliente;
+
+    @NotBlank(message = "El email no puede estar vacío")
+    @Email(message = "El formato de email es inválido")
+    @Size(max = 150, message = "El email no debe exceder los 150 caracteres")
+    @Column(name = "email", length = 150, nullable = false, unique = true)
+    private String email;
+
+    @NotBlank(message = "La contraseña no puede estar vacía")
+    @Size(max = 200, message = "La contraseña no debe exceder los 200 caracteres")
+    @Column(name = "contrasena", length = 200, nullable = false)
+    private String contrasena;
+
+    @NotBlank(message = "El nombre no puede estar vacío")
+    @Size(max = 45, message = "El nombre no debe exceder los 45 caracteres")
+    @Column(name = "nombre", length = 45, nullable = false)
+    private String nombre;
+
+    @NotBlank(message = "Los apellidos no pueden estar vacíos")
+    @Size(max = 100, message = "Los apellidos no deben exceder los 100 caracteres")
+    @Column(name = "apellidos", length = 100, nullable = false)
+    private String apellidos;
+
+    @NotBlank(message = "La dirección no puede estar vacía")
+    @Size(max = 200, message = "La dirección no debe exceder los 200 caracteres")
+    @Column(name = "direccion", length = 200, nullable = false)
+    private String direccion;
+
+    @Column(name = "telefono", nullable = false)
+    private Integer telefono;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "fechaNacimiento", nullable = false)
+    private Date fechaNacimiento;
+}
