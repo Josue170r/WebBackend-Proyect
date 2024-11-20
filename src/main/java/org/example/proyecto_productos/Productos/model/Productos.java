4ed168f1a79508592ad25eaf2030273b9496fb9e
Carlos Moreno <cmorenoh2000@alumno.ipn.mx>
2024-11-19 23:17:46 -0600

    Unión de tablas Proveedores y Productos

--- src/main/java/org/example/proyecto_productos/Productos/model/Productos.java
+++ src/main/java/org/example/proyecto_productos/Productos/model/Productos.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.example.proyecto_productos.Proveedores.models.Proveedores;
+
 import java.io.Serializable;
 
 @Data
