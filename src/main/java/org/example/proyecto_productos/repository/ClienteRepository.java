74a13ce73c2e2c89b09ff6b81214fd8cc9c8e497
JerryOrtiz <mruriel982@gmail.com>
2024-11-21 23:43:07 -0600

    Se agrega el codigo para CategoriaController

--- src/main/java/org/example/proyecto_productos/repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.proyecto_productos.repository;
-
-
-import org.example.proyecto_productos.model.Cliente;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClienteRepository extends CrudRepository<Cliente, Integer> {
-}
