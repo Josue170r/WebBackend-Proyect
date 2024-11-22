e8fd66f4b83654ff2299753f183f617d77b1b2c5
JerryOrtiz <mruriel982@gmail.com>
2024-11-21 21:40:56 -0600

    Se agrega package con la tabla clientes

---
+++ src/main/java/org/example/proyecto_productos/Clientes/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,37 @@
+package org.example.proyecto_productos.Clientes.service.impl;
+
+import org.example.proyecto_productos.Clientes.model.Cliente;
+import org.example.proyecto_productos.Clientes.repository.ClienteRepository;
+import org.example.proyecto_productos.Clientes.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClienteServiceImpl implements ClienteService {
+
+    @Autowired
+    private ClienteRepository repository;
+
+    @Override
+    public List<Cliente> readAllClientes() {
+        return (List<Cliente>) repository.findAll();
+    }
+
+    @Override
+    public Cliente readCliente(Integer id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Cliente createCliente(Cliente cliente) {
+        return repository.save(cliente);
+    }
+
+    @Override
+    public void deleteCliente(Integer id) {
+        repository.deleteById(id);
+    }
+}
+
