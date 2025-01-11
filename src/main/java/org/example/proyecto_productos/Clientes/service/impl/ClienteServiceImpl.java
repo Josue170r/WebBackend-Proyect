719415d0ffc420008b14b057b4fe62be5b28dda9
Josue1707 <josuemonro@gmail.com>
2025-01-10 23:49:43 -0600

    Setup login and singup nedpoints

--- src/main/java/org/example/proyecto_productos/Clientes/service/impl/ClienteServiceImpl.java
+++ src/main/java/org/example/proyecto_productos/Clientes/service/impl/ClienteServiceImpl.java
@@ -1,48 +1,64 @@
 package org.example.proyecto_productos.Clientes.service.impl;
 
-import org.example.proyecto_productos.Clientes.utils.Utils;
+import org.example.proyecto_productos.Clientes.repository.ClienteRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.transaction.annotation.Transactional;
 import org.example.proyecto_productos.Clientes.model.Cliente;
-import org.example.proyecto_productos.Clientes.repository.ClienteRepository;
 import org.example.proyecto_productos.Clientes.service.ClienteService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClienteServiceImpl implements ClienteService {
 
-    @Autowired
-    private ClienteRepository repository;
-    @Autowired
-    private Utils utils;
+    private final ClienteRepository repository;
+    public ClienteServiceImpl(ClienteRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Cliente> user = repository.findByUserName(username);
+        if (user.isPresent()) {
+            Cliente objUser = user.get();
+            return User.builder()
+                    .username(objUser.getUserName())
+                    .password(objUser.getContrasena())
+                    .build();
+        } else {
+            throw new UsernameNotFoundException(username);
+        }
+    }
 
     @Override
     @Transactional(readOnly = true)
     public List<Cliente> readAllClientes() {
-        return (List<Cliente>) repository.findAll();
+        return repository.findAll();
     }
 
     @Override
     @Transactional(readOnly = true)
     public Cliente readCliente(Long id) {
-        return repository.findById(id).orElse(null);
+        return repository.findById(id).orElseThrow(
+            () -> new RuntimeException("Usuario no encontrado")
+        );
     }
 
     @Override
     @Transactional
     public Cliente createCliente(Cliente cliente) {
-        String codifiedPass = utils.encriptar(cliente.getContrasena());
-        cliente.setContrasena(codifiedPass);
         return repository.save(cliente);
     }
 
     @Override
-    public Cliente actualizarCliente(Cliente cliente) {
-        String codifiedPass = utils.encriptar(cliente.getContrasena());
-        cliente.setContrasena(codifiedPass);
-        return repository.save(cliente);
+    public Cliente findByUsername(String username) {
+        return repository.findByUserName(username).orElseThrow(
+            () -> new RuntimeException("Usuario no encontrado")
+        );
     }
 
     @Transactional
